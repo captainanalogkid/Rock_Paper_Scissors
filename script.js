380dad360a78776754f46b50f87d0a90d3994550
Jonathan Dunn <115617267+captainanalogkid@users.noreply.github.com>
2024-11-14 10:55:48 -0500

    modified:   script.js added functions for human and computer choice, switched order of human and computer choices (human first)

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let humanChoices = ("rock", "paper", "scissors");
+let humanPrompt = prompt("Make Your Selection (rock, paper, or scissors):");
+function getHumanChoice() {
+
+    if (humanChoices.includes(humanPrompt)) {
+        console.log("Your Choice Is: ${humanPrompt}");
+    }
+    else {
+        console.log("Choose again");
+    }
+}
+console.log(humanPrompt);
+
+function getComputerChoice() {
+    const computerIndex = Math.floor(Math.random() * 3);
+   switch (computerIndex) {
+    case 0: 
+        return "rock";
+    case 1: 
+        return "paper";
+    case 2:
+        return "scissors";
+   }
+}
+console.log(getComputerChoice());
+
+
+
+
+
+
 
