c3251ba09981ce7a116936c23686794ecd58d998
Jonathan Dunn <115617267+captainanalogkid@users.noreply.github.com>
2024-11-15 20:07:10 -0500

    added playRound function. needs fixing as returning else statement even when human wins. also need to inincrement score variable after each round#

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let humanChoices = ("rock", "paper", "scissors");
 let humanPrompt = prompt("Make Your Selection (rock, paper, or scissors):");
+
+
+
+// variables to hold scores //
+let humanScore = 0;
+let computerScore = 0;
+
 function getHumanChoice() {
 
     if (humanChoices.includes(humanPrompt)) {
-        console.log("Your Choice Is: ${humanPrompt}");
-    }
-    else {
-        console.log("Choose again");
+        return humanPrompt; 
     }
 }
 console.log(humanPrompt);
 
+const computerIndex = Math.floor(Math.random() * 3);
+
 function getComputerChoice() {
-    const computerIndex = Math.floor(Math.random() * 3);
    switch (computerIndex) {
     case 0: 
         return "rock";
@@ -22,7 +27,31 @@ function getComputerChoice() {
         return "scissors";
    }
 }
-console.log(getComputerChoice());
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+
+// function to play single round. Need to fix result. console only returning computer wins /
+function playRound(humanChoice, computerChoice) {
+    if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        return "Human Wins!";
+    }
+    if (humanChoice === computerChoice) {
+        return "It's a Draw!";
+    }
+    else {
+        return "Computer Wins!"
+    }
+}
+
+// calling the function
+const result = playRound(humanChoice, computerChoice);
+console.log(result);
 
 
 
